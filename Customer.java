7f8fff04c998dc8846d1b1809845f24000737ccc
SuHwan An <127846128+paven-090214@users.noreply.github.com>
2023-11-16 17:20:09 +0900

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,14 @@
+
+public class Customer {
+	int cusNo;
+	int mile;
+	
+	public Customer(int cusNo, int mile) {
+		this.cusNo = cusNo;
+		this.mile = mile;
+	}
+	public static void main(String[] args) {
+
+	}
+
+}
